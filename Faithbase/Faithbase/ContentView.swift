d78a0fcb0a98a17e11b4b056fc98e84b3d14ae33
Constantin Saulenco <constantin.saulenco@appointfix.com>
2024-10-26 19:45:37 +0300

    added repo and endpoint

--- Faithbase/Faithbase/ContentView.swift
+++ Faithbase/Faithbase/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let endpoint = Endpoint()
+    @State var reply: String = ""
+    @State var medic: String = ""
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(reply)
+            Text(medic)
+            Button("Get recommendation") {
+                getMedic()
+            }
         }
         .padding()
+    }
+
+    private func getMedic() {
+
+        let symptoms = "I have been experiencing persistent headaches, dizziness, and occasional numbness in my hands."
+        let prompt = """
+        The user will provide a description of their symptoms. Please respond by recommending the type of medical specialist they should consult, using the json format:
+        "{message: Based on your symptoms, it would be best to consult a [specialist type],
+           speciality: speciality
+         }".
+
+        Consider common medical specialties, such as cardiology, dermatology, gastroenterology, neurology, orthopedics, and psychiatry, and choose the one that best fits the user's symptoms.
+
+        User's Symptoms: \(symptoms)
+        """
+
+        endpoint.makeOpenAIRequest(prompt: prompt) { response in
+            self.reply = response?.message ?? ""
+            let medic = MedicsRepo.getMedics(response?.speciality ?? "")
+            self.medic = medic?.name ?? "Not found"
+        }
     }
 }
 
