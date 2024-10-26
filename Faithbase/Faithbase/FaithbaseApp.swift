5dffbfcc4da241d4de2dd2de8ca8b723560e80c8
Constantin Saulenco <constantin.saulenco@appointfix.com>
2024-10-26 16:45:18 +0300

    project start

---
+++ Faithbase/Faithbase/FaithbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaithbaseApp.swift
+//  Faithbase
+//
+//  Created by Constantin Saulenco on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FaithbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
